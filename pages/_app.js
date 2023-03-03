487a982b38f1a88a7241ccf109909f30997fcd5f
Sebastian Orellana <p.sebastianorellana@gmail.com>
2023-03-03 18:04:17 -0500

    chore: added chat engine api

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,10 @@ import "../styles/auth.css"
 import "../styles/chats.css"
 import "../styles/index.css"
 
+import { ContextProvider } from "../context"
+
 export default function App({Component, pageProps}){
-    return <Component {...pageProps}/>
+    return <ContextProvider>
+            <Component {...pageProps}/>
+        </ContextProvider>
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,38 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import {Context} from '../context';
+import axios from 'axios';
+import { useRouter } from "next/router";
+
+const putApi = 'https://api.chatengine.io/users';
 
 export default function Auth() {
-  return <div className="background">auth</div>;
+  const router = useRouter();
+  const ctx = useContext(Context);
+  const {setUsername, username, secret, setSecret} = ctx;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(username.length ===0 || secret.length === 0) return
+
+    axios.put(putApi, {username, secret}, {headers: {"Private-key": process.env.NEXT_PUBLIC_CHAT_API}}).then(res=> router.push('/chats') );
+  }
+  return <div className="background">
+
+    <div className="auth-container">
+      <form className="auth-form" onSubmit={handleSubmit}>
+        <div className="auth-title">NextJS Chat</div>
+        <div className="input-container">
+          <input placeholder="Email" className="text-input" onChange={e=> setUsername(e.target.value)}/>
+        </div>
+        <div className="input-container">
+          <input placeholder="Password" type="password" className="text-input" onChange={e=> setSecret(e.target.value)}/>
+        </div>
+        <button type="submit" className="submit-button">
+          Login / Sign Up
+        </button>
+      </form>
+    </div>
+  </div>;
 }
