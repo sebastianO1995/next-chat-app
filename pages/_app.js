66d799242dc84ff8a58dcd1e34e67cbfdf8cda66
Sebastian Orellana <p.sebastianorellana@gmail.com>
2023-03-03 16:39:05 -0500

    chore: set up project

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../styles/auth.css"
+import "../styles/chats.css"
+import "../styles/index.css"
+
+export default function App({Component, pageProps}){
+    return <Component {...pageProps}/>
+}
